0cfa581a64760b49371e2aaf965d7f39b455c211
DauweQuinten <quinten.dauwe@student.howest.be>
2022-05-24 13:15:16 +0200

    feat: html boilerplate

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import time
+import serial
 from RPi import GPIO
 from helpers.klasseknop import Button
 import threading
@@ -9,38 +10,35 @@ from flask import Flask, jsonify
 from repositories.DataRepository import DataRepository
 
 from selenium import webdriver
-
 # from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options
 
 
-ledPin = 21
-btnPin = Button(20)
-
 # Code voor Hardware
-def setup_gpio():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(ledPin, GPIO.OUT)
-    GPIO.output(ledPin, GPIO.LOW)
-    
-    btnPin.on_press(lees_knop)
+ser = serial.Serial('/dev/ttyS0')
 
 
-def lees_knop(pin):
-    if btnPin.pressed:
-        print("**** button pressed ****")
-        if GPIO.input(ledPin) == 1:
-            switch_light({'lamp_id': '3', 'new_status': 0})
-        else:
-            switch_light({'lamp_id': '3', 'new_status': 1})
+def get_distance_data():
+    data_bytes = []
+    data = ser.read()
+    int_data = int.from_bytes(data, "big")
+
+    if int_data == 0xff:
+        data_bytes.append(int_data)
+
+        for i in range(3):
+            int_data = int.from_bytes(ser.read(), "big")
+            data_bytes.append(int_data)
+        return data_bytes
 
 
+def get_distance_value(data):
+    data_h = data[1]
+    data_l = data[2]
+    return (data_h << 8) | data_l
 
 
 # Code voor Flask
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'geheim!'
 socketio = SocketIO(app, cors_allowed_origins="*", logger=False,
@@ -54,7 +52,6 @@ def error_handler(e):
     print(e)
 
 
-
 # API ENDPOINTS
 
 
@@ -66,48 +63,6 @@ def hallo():
 @socketio.on('connect')
 def initial_connection():
     print('A new client connect')
-    # # Send to the client!
-    # vraag de status op van de lampen uit de DB
-    status = DataRepository.read_status_lampen()
-    emit('B2F_status_lampen', {'lampen': status}, broadcast=True)
-
-
-@socketio.on('F2B_switch_light')
-def switch_light(data):
-    # Ophalen van de data
-    lamp_id = data['lamp_id']
-    new_status = data['new_status']
-    print(f"Lamp {lamp_id} wordt geswitcht naar {new_status}")
-
-    # Stel de status in op de DB
-    res = DataRepository.update_status_lamp(lamp_id, new_status)
-
-    # Vraag de (nieuwe) status op van de lamp en stuur deze naar de frontend.
-    data = DataRepository.read_status_lamp_by_id(lamp_id)
-    socketio.emit('B2F_verandering_lamp', {'lamp': data}, broadcast=True)
-
-    # Indien het om de lamp van de TV kamer gaat, dan moeten we ook de hardware aansturen.
-    if lamp_id == '3':
-        print(f"TV kamer moet switchen naar {new_status} !")
-        GPIO.output(ledPin, new_status)
-
-
-
-# START een thread op. Belangrijk!!! Debugging moet UIT staan op start van de server, anders start de thread dubbel op
-# werk enkel met de packages gevent en gevent-websocket.
-def all_out():
-    while True:
-        print('*** We zetten alles uit **')
-        DataRepository.update_status_alle_lampen(0)
-        GPIO.output(ledPin, 0)
-        status = DataRepository.read_status_lampen()
-        socketio.emit('B2F_status_lampen', {'lampen': status})
-        time.sleep(15)
-
-def start_thread():
-    print("**** Starting THREAD ****")
-    thread = threading.Thread(target=all_out, args=(), daemon=True)
-    thread.start()
 
 
 def start_chrome_kiosk():
@@ -117,7 +72,8 @@ def start_chrome_kiosk():
     options = webdriver.ChromeOptions()
     # options.headless = True
     # options.add_argument("--window-size=1920,1080")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--allow-running-insecure-content')
     options.add_argument("--disable-extensions")
@@ -128,7 +84,7 @@ def start_chrome_kiosk():
     # options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.add_argument('--kiosk')
-    # chrome_options.add_argument('--no-sandbox')         
+    # chrome_options.add_argument('--no-sandbox')
     # options.add_argument("disable-infobars")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
@@ -141,23 +97,22 @@ def start_chrome_kiosk():
 
 def start_chrome_thread():
     print("**** Starting CHROME ****")
-    chromeThread = threading.Thread(target=start_chrome_kiosk, args=(), daemon=True)
+    chromeThread = threading.Thread(
+        target=start_chrome_kiosk, args=(), daemon=True)
     chromeThread.start()
-
 
 
 # ANDERE FUNCTIES
 
-
 if __name__ == '__main__':
     try:
-        setup_gpio()
-        start_thread()
+        # setup_gpio()
+        # start_thread()
         start_chrome_thread()
         print("**** Starting APP ****")
         socketio.run(app, debug=False, host='0.0.0.0')
     except KeyboardInterrupt:
-        print ('KeyboardInterrupt exception is caught')
+        print('KeyboardInterrupt exception is caught')
     finally:
-        GPIO.cleanup()
-
+        pass
+        # GPIO.cleanup()
