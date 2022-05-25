cfd836b661e48ff2b9c242ed9a3944d871b5dae8
DauweQuinten <quinten.dauwe@student.howest.be>
2022-05-25 16:09:03 +0200

    feat: read device methods

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -9,3 +9,14 @@ class DataRepository:
         else:
             gegevens = request.form.to_dict()
         return gegevens
+
+    @staticmethod
+    def read_one_device(deviceid):
+        sql = "SELECT * FROM device WHERE deviceid = %s"
+        params = [deviceid]
+        return Database.get_one_row(sql, params)
+
+    @staticmethod
+    def read_all_devices():
+        sql = "SELECT * FROM device"
+        return Database.get_rows(sql)
