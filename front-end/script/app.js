bf00993bc9a8d4007cfa78c136bd079c6ab57493
DauweQuinten <quinten.dauwe@student.howest.be>
2022-06-08 15:26:44 +0200

    feat: apexchart en navigatie

--- front-end/script/app.js
+++ front-end/script/app.js
@@ -14,6 +14,94 @@ const showDistance = function (payload) {
   htmlDistance.innerHTML = `${distance} mm`;
 };
 
+const drawChart = function () {
+  var options = {
+    series: [70],
+    chart: {
+      height: 350,
+      type: 'radialBar',
+      toolbar: {
+        show: true,
+      },
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24,
+          },
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35,
+          },
+        },
+
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: '#888',
+            fontSize: '17px',
+          },
+          value: {
+            formatter: function (val) {
+              return parseInt(val);
+            },
+            color: '#111',
+            fontSize: '36px',
+            show: true,
+          },
+        },
+      },
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'horizontal',
+        shadeIntensity: 0.5,
+        gradientToColors: ['#ABE5A1'],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100],
+      },
+    },
+    stroke: {
+      lineCap: 'round',
+    },
+    labels: ['Percent'],
+  };
+
+  var chart = new ApexCharts(
+    document.querySelector('.js-level-chart'),
+    options
+  );
+  chart.render();
+};
+
 // #endregion
 
 // #region ***  Callback-No Visualisation - callback___  ***********
@@ -47,6 +135,7 @@ const listenToSocket = function () {
 document.addEventListener('DOMContentLoaded', function () {
   console.info('DOM geladen');
   listenToSocket();
+  drawChart();
 });
 
 // #endregion
